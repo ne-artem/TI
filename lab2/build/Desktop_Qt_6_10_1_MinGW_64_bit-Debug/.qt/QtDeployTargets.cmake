0cc86ec2fc544a3b1cc7ef481cfc674d7b4a5607
ne-artem <nikitosivanov109@gmail.com>
2026-03-29 18:29:51 +0300

    delete dir lab2

--- lab2/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_lab2_FILE D:/UCHEBA/TI/lab2/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/lab2.exe)
-set(__QT_DEPLOY_TARGET_lab2_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_lab2_RUNTIME_DLLS C:/Qtl/6.10.1/mingw_64/bin/Qt6Widgets.dll;C:/Qtl/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/Qtl/6.10.1/mingw_64/bin/Qt6Core.dll)
--- lab2/lfsrs.cpp
+++
@@ -1,3 +0,0 @@
-#include "lfsrs.h"
-
-LFSRS::LFSRS() {}
